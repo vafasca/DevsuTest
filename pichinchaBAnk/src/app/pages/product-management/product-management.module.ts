cdbc843ea6bddc884d182ba1b0a19c3cf396f40d
Bruno Alejandro Ortiz Velasquez <counter_19_95@hotmail.com>
2024-02-17 13:30:41 -0400

    Added header component with project-wide availability and styling

--- pichinchaBAnk/src/app/pages/product-management/product-management.module.ts
+++ pichinchaBAnk/src/app/pages/product-management/product-management.module.ts
@@ -7,6 +7,7 @@ import { ProductListComponent } from './shared/product-list/product-list.compone
 import { SearchProductComponent } from './shared/search-product/search-product.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RegistrationFormComponent } from './shared/registration-form/registration-form.component';
+import { HeaderComponent } from 'src/app/shared/header/header.component';
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { RegistrationFormComponent } from './shared/registration-form/registrati
     ProductManagementComponent,
     ProductListComponent,
     SearchProductComponent,
-    RegistrationFormComponent
+    RegistrationFormComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
