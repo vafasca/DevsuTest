27aba5d08ea653abcde5fda29e0a916329b31a1e
Bruno Alejandro Ortiz Velasquez <counter_19_95@hotmail.com>
2024-02-18 15:43:56 -0400

    Updated HTML and date registration to automatically generate 'date_revision' and lock it for editing

--- pichinchaBAnk/src/app/pages/product-management/shared/registration-form/registration-form.component.ts
+++ pichinchaBAnk/src/app/pages/product-management/shared/registration-form/registration-form.component.ts
@@ -24,6 +24,13 @@ export class RegistrationFormComponent implements OnInit {
   /** ID del producto */
   productId!: string;
 
+  /**
+   * Constructor del componente.
+   * @param productListSvc Servicio para operaciones relacionadas con productos.
+   * @param router Servicio para la navegación entre rutas.
+   * @param route Servicio para obtener información sobre la ruta activa.
+   * @param toastr Servicio para mostrar notificaciones Toastr.
+   */
   constructor(
     private productListSvc: ProductApiService,
     private router: Router,
@@ -31,6 +38,10 @@ export class RegistrationFormComponent implements OnInit {
     private toastr: ToastrService
   ) {}
 
+  /**
+   * Método de inicialización del componente.
+   * Suscribe a los parámetros de la ruta y establece el formulario de registro.
+   */
   ngOnInit(): void {
     // Suscripción a los parámetros de la ruta
     this.route.params.subscribe((params) => {
@@ -59,12 +70,41 @@ export class RegistrationFormComponent implements OnInit {
       ]),
       logo: new FormControl(null, Validators.required),
       date_release: new FormControl(null, [Validators.required]),
-      date_revision: new FormControl(null, [Validators.required]),
+      date_revision: new FormControl(null),
+    });
+
+    // Suscribirse a los cambios en la fecha de liberación para calcular la fecha de revisión
+    this.registrationForm.get('date_release')!.valueChanges.subscribe(() => {
+      this.calculateRevisionDate();
     });
   }
 
+  /**
+   * Método para calcular la fecha de revisión.
+   */
+  calculateRevisionDate(): void {
+    const releaseDate = this.registrationForm.get('date_release')!.value;
+    if (releaseDate) {
+      const newRevisionDate = new Date(releaseDate);
+      newRevisionDate.setFullYear(newRevisionDate.getFullYear() + 1);
+      this.registrationForm.get('date_revision')!.setValue(newRevisionDate.toISOString().split('T')[0]);
+    }
+  }
+
+  /**
+   * Verifica si el formulario está en modo edición.
+   * @returns Verdadero si el formulario está en modo edición, falso de lo contrario.
+   */
   isEditMode(): boolean {
     return !!this.productId;
+  }
+
+  /**
+   * Verifica si el formulario está en modo edición de la fecha.
+   * @returns Verdadero siempre, ya que el formulario está siempre en modo edición de la fecha.
+   */
+  isEditModeDate(): boolean {
+    return true;
   }
 
   /**
@@ -137,7 +177,7 @@ export class RegistrationFormComponent implements OnInit {
   }
 
   /**
-   * Maneja el envio de datos a actualizar o registrar del formulario.
+   * Maneja el envío de datos a actualizar o registrar del formulario.
    */
   onSubmit(): void {
     if (this.productId) {
